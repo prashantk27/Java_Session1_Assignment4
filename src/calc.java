a424fc3901c41962d16e7f4a467c9cb41d3c52bd
prashantk27 <rockingwrickindia@gmail.com>
2016-09-21 12:18:32 +0530

    Submit

---
+++ src/calc.java
@@ -0,0 +1,21 @@
+
+public class calc {
+	public static void main(String[] agrs){
+		int x=5, y=10, calculate1, calculate2, calculate3, calculate4;
+		
+			calculate1= x+y*2;
+			System.out.println(calculate1);
+			
+			calculate2= x-y+2;
+			System.out.println(calculate2);
+			
+			calculate2= x-(y+2);
+			System.out.println(calculate2);
+			
+			calculate3= (x+y)*2;
+			System.out.println(calculate3);
+			
+			calculate4= y%x;
+			System.out.println(calculate4);
+	}
+}
